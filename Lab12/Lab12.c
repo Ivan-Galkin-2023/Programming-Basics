96f367f5c45a9609efecfae84d2630f62aaff3d2
Андрей Боев <a.boev.it@gmail.com>
2024-01-16 17:25:17 +0300

    Fix Lab12

--- Lab12/Lab12.c
+++ Lab12/Lab12.c
@@ -4,6 +4,11 @@
 int main(int argc, char* argv[])
 {
     int a, b;
+    if (argc < 3)
+    {
+        printf("Argument count exception.");
+        return -1;
+    }
     freopen("output.txt", "w", stdout);
     sscanf_s(argv[1], "%d", &a);
     sscanf_s(argv[2], "%d", &b);
